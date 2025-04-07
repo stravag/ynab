939271f1b7341741ea0ac229dbdadb115de8c405
Ranil Wijeyratne <me@ranil.ch>
2025-04-07 20:20:29 +0200

    improve ui

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,18 @@
 import {handleFile} from "./fileHandler.ts";
 
-const dropZone = document.querySelector<HTMLDivElement>('#drop-zone')!
-dropZone.addEventListener('dragover', (event) => {
-    event.preventDefault()
-    dropZone.style.backgroundColor = 'gray'
-})
+const dropZone = document.querySelector<HTMLElement>('#file-input')!
+const input = document.querySelector<HTMLInputElement>('#file')!
 
-dropZone.addEventListener('dragleave', (event) => {
-    event.preventDefault()
-    dropZone.style.backgroundColor = ''
-})
-
+dropZone.addEventListener('dragover', e => e.preventDefault())
 dropZone.addEventListener('drop', (event) => {
     event.preventDefault()
-    dropZone.style.backgroundColor = ''
     if (event.dataTransfer) {
         handleFile(event.dataTransfer.files[0])
     }
 })
+
+input.addEventListener('change', (_) => {
+    if (input.files) {
+        handleFile(input.files[0])
+    }
+});
